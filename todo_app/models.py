55772b36957f1210966467436d4595a0fadceade
teju159 <tejaswinik1826@gmail.com>
2024-09-28 14:35:37 +0530

    first commit

---
+++ todo_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    time_requried=models.IntegerField(null="False")
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
